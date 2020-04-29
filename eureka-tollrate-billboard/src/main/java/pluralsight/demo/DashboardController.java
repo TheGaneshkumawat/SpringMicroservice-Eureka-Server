a39787054d597d8a6728433c4848938d2c3e8607
TheGaneshkumawat <kumawat.ganesh12@gmail.com>
2020-04-29 11:34:55 +0530

    Initial commit

---
+++ eureka-tollrate-billboard/src/main/java/pluralsight/demo/DashboardController.java
@@ -0,0 +1,34 @@
+package pluralsight.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class DashboardController {
+	
+	
+	@LoadBalanced
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();		
+	}
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@RequestMapping("/dashboard")
+	public String GetTollRate(@RequestParam int stationId, Model m) {
+		
+		TollRate tr = restTemplate.getForObject("http://tollrate-service/tollrate/" + stationId, TollRate.class);
+		System.out.println("stationId: " + stationId);
+		m.addAttribute("rate", tr.getCurrentRate());
+		return "dashboard";
+	}
+}
